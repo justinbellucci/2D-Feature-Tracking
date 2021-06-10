ced72477c367f0397e9e46be7791baf95c8d0566
justinbellucci <justin.bellucci@me.com>
2021-06-10 12:37:34 -0600

    Harris detector guts implemented

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -83,7 +83,8 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        string detectorType = "SHITOMASI";
+        string detectorType = "HARRIS"; // SHITOMASI, HARRIS
+        // FAST, ORB, KAZE, SIFT
 
         //// STUDENT ASSIGNMENT
         //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
@@ -93,9 +94,13 @@ int main(int argc, const char *argv[])
         {
             detKeypointsShiTomasi(keypoints, imgGray, false);
         }
+        else if (detectorType.compare("HARRIS") == 0)
+        {
+            detKeypointsHarris(keypoints, imgGray, true);
+        }
         else
         {
-            //...
+
         }
         //// EOF STUDENT ASSIGNMENT
 
